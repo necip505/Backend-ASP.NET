9871c131301a8c7556ea6e5fc8185d4c8d31cdbf
NecipTurkmenoglu <87278594+necip505@users.noreply.github.com>
2025-03-26 19:06:06 +0300

    initial

---
+++ api/Models/Comment.cs
@@ -0,0 +1,11 @@
+namespace api.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public int? StockId { get; set; }
+        public Stock? Stock { get; set; }
+    }
+}
